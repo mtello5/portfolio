83f3329d9a972c70754707e5c3eb2222d4395b99
mtello5 <78052803+mtello5@users.noreply.github.com>
2023-07-12 16:15:40 -0700

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,14 +6,14 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
-  middleName: "",
-  lastName: "Shoaib",
+  firstName: "Matthew",
+  middleName: "A",
+  lastName: "Tello",
   message: " Passionate about changing the world with technology. ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/mtello5",
     },
     {
       image: "fa-facebook",
